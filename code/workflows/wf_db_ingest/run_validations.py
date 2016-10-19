58c60732d663ccd5af3a619aa8582434e629e035
Ram Manohar Bheemana <rbheemana@users.noreply.github.com>
2016-10-19 13:33:33 -0400

    Create run_validations.py

---
+++ code/workflows/wf_db_ingest/run_validations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# Purpose: This Job is the starting point of all jobs. 
+#          Looks up jobnames.list and calls the script
+
+import sys, os, commands, datetime, time ,getpass, errno
+from optparse import OptionParser
+import subprocess
+from subprocess import Popen, PIPE
+
+def main():
+	print("Started Run_validations.py")
+
+if __name__ == "__main__":
+    main()
